af844b4971b30a9d03fc5f1e80becc404c285a07
Alex Palmada <alexpalmadabarbosa@girona.lasalle.cat>
2025-05-13 11:23:56 +0200

    Doing the Serialization

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEditor;
+using Unity.VisualScripting;
+using UnityEditor.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,26 +16,32 @@ public class GameManager : MonoBehaviour
     private int score = 0;
     private int lives = 0;
     public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI gameOverText;
     public TextMeshProUGUI healthText;
     public Button restartButton;
     public GameObject titleScreen;
+    public GameObject gameOverScreen;
     public bool isPaused = false;
     public GameObject pauseMenu;
+    public GameObject inputField;
+    public GameObject nameText;
+    private string nameScore;
+    private SaveData leaderboard;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused)
+        if (!titleScreen.activeSelf && isGameActive) { 
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                Resume();
-                isPaused = false;
-            }
-            else { 
-                Pause();
-                isPaused=true;
+                if (isPaused)
+                {
+                    Resume();
+                    isPaused = false;
+                }
+                else { 
+                    Pause();
+                    isPaused=true;
+                }
             }
         }
     }
@@ -84,8 +92,9 @@ public class GameManager : MonoBehaviour
     {
         Debug.Log("Game Over");
         isGameActive = false;
-        gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(true);
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        gameOverScreen.SetActive(true);
     }
 
     public void UpdateScore(int scoreToAdd)
@@ -105,5 +114,13 @@ public class GameManager : MonoBehaviour
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+    public void ReadInput()
+    {
+        nameScore = nameText.GetComponent<TextMeshProUGUI>().text;
+        Debug.Log("Text have been saved as "+ "\""+nameScore+"\"");
+        inputField.SetActive(false);
+        leaderboard.Add(name, score);
     }
 }
---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class SaveData : MonoBehaviour
+{
+    private string savedName;
+    private int savedScore;
+
+    public void Add(string name, int score)
+    {
+
+    }
+    private void NewDirectory()
+    {
+
+    }
+}
